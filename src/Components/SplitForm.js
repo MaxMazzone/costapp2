b3d975aa7da1c92297cde62c3edaba32f628136e
stgeorge3 <amaxmazzone@gmail.com>
2020-05-20 14:29:39 -0400

    lint

---
+++ src/Components/SplitForm.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { useFormik } from "formik"
+
+function SplitForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default SplitForm
