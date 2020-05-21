54190187a0f152fa48a0fb665efd2875e4f3d0b1
stgeorge3 <amaxmazzone@gmail.com>
2020-05-21 16:28:16 -0400

    add form elements

--- src/Components/SplitForm.js
+++ src/Components/SplitForm.js
@@ -4,7 +4,10 @@ import { useFormik } from "formik"
 function SplitForm() {
   const formik = useFormik({
     initialValues: {
-      email: "",
+      costOfBottle: 0,
+      sizeOfBottle: 0,
+      amountUsed: 0,
+      units: "OZ",
     },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
@@ -13,13 +16,40 @@ function SplitForm() {
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email Address</label>
+        <label htmlFor="cost-of-bottle">Cost of Bottle</label>
         <input
-          id="email"
-          name="email"
-          type="email"
+          id="costOfBottle"
+          name="costOfBottle"
+          type="number"
           onChange={formik.handleChange}
-          value={formik.values.email}
+          value={formik.values.costOfBottle}
+        />
+        <select
+          id="units"
+          name="units"
+          type="select"
+          onBlur={formik.handleChange}
+          value={formik.values.unit}
+        >
+          <option value="OZ">OZ</option>
+          <option value="ML">ML</option>
+        </select>
+
+        <label htmlFor="size-of-bottle">Size of Bottle</label>
+        <input
+          id="sizeOfBottle"
+          name="sizeOfBottle"
+          type="number"
+          onChange={formik.handleChange}
+          value={formik.values.sizeOfBottle}
+        />
+        <label htmlFor="amount-used">Amount Used</label>
+        <input
+          id="amountUsed"
+          name="amountUsed"
+          type="number"
+          onChange={formik.handleChange}
+          value={formik.values.amountUsed}
         />
         <button type="submit">Submit</button>
       </form>
